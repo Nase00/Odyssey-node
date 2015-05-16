89f5a865fb6fa3254f20dc87d83e66f72e4794b9
Sean <nase00@gmail.com>
2015-05-16 18:42:45 -0500

    Remove comments.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var Path = require('path'),
     Hapi = require('hapi'),
     Good = require('good'),
-    Mongoose = require('mongoose');
-    // db = require('./config/db');
+    Mongoose = require('mongoose'),
+    db = require('./config/db');
 
-// MongoDB Connection
 Mongoose.connect('mongodb://localhost');
 
 var routes = require('./routes/index');
@@ -31,12 +30,10 @@ server.register({
   }
 }, function (err) {
   if (err) {
-    throw err; // something bad happened loading the plugin
+    throw err;
   }
 });
 
 server.start(function() {
   console.log('Running at ' + server.info.uri)
 });
-
-// Mongoose.connection.db.dropDatabase();
