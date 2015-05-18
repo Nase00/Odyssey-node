eff4b79622c7fe9266c22d8e7a2ede13f994d1a0
Nase00 <nase00@gmail.com>
2015-05-18 14:34:48 -0500

    Auto every station file.

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ```
 git clone git@github.com:Nase00/odyssey-redirect.git
 npm install
-node seed stations 0 // increments to 1
+node seed stations
 node seed trips 0 // increments to 16
 ```
--- tasks/stations.js
+++ tasks/stations.js
@@ -8,26 +8,28 @@ var config = require('../data/csv/config');
 module.exports = function() {
   Mongoose.connect('mongodb://localhost');
   var collection = [];
-	var stream = fs.createReadStream(config.files.stations[0]),
-        csvStream = csv(config.options.stations)
-      .on("data", function(data){
-        collection.push(data);
-        console.log("Parsing station #" + data.trip_id)
-      })
-      .on("end", function() {
-        collection.map(function(data) {
-          var station = new models.Station({
-            stationId: data.id,
-            name: data.name,
-            lat: data.latitude,
-            lng: data.longitude
+  for (var i = 0; i < config.files.stations.length; i++) {
+  	var stream = fs.createReadStream(config.files.stations[i]),
+          csvStream = csv(config.options.stations)
+        .on("data", function(data){
+          collection.push(data);
+          console.log("Parsing station #" + data.trip_id)
+        })
+        .on("end", function() {
+          collection.map(function(data) {
+            var station = new models.Station({
+              stationId: data.id,
+              name: data.name,
+              lat: data.latitude,
+              lng: data.longitude
+            });
+            station.save(function (err) {
+              console.log("Saved " + station)
+              if (err) console.log(err);
+            })
           });
-          station.save(function (err) {
-            console.log("Saved " + station)
-            if (err) console.log(err);
-          })
         });
-      });
-    stream.pipe(csvStream);
+      stream.pipe(csvStream);
+  }
     // Mongoose.connection.close();
 }
