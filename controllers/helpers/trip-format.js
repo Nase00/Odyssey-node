3e02752a106f9c4517a6d16f85530c9a588f2051
Nase00 <nase00@gmail.com>
2015-05-19 10:09:05 -0500

    Set proper responses.

---
+++ controllers/helpers/trip-format.js
@@ -0,0 +1,43 @@
+var models = require('../../models/index');
+
+var findTrip = function(req, reply) {
+	// Lookup bike's trips
+	models.Trip.findOne({bikeId: req.params.bikeId})
+				.sort('-startTime')
+				.skip(req.params.offset)
+				.limit(1)
+				.exec(function(err, trip) {
+					// Bike must have zero trips
+					if (!trip && req.params.offset == 0) {
+						reply({
+							status: 404,
+							message: "No trips found"
+						}) 
+					// Bike may have trips, but not at offset parameter
+					} else if (!trip) {
+						reply({
+							status: 510,
+							message: "End of the line"
+						})
+					// Return trip with station coordinates
+					} else {
+						models.Station.findOne({stationId: trip.originStationId})
+													.limit(1)
+													.exec(function(err, station) {
+														reply({
+															"status": 200,
+															"lat": station.lat,
+															"lng": station.lng,
+															"trip_id": trip.tripId,
+															"start_time": trip.startTime,
+															"stop_time": trip.stopTime,
+															"duration": trip.duration,
+															"start_location": trip.originStationName,
+															"stop_location": trip.destinationStationName
+														})
+													});
+					}
+				})
+};
+
+module.exports = findTrip;
--- controllers/index.js
+++ controllers/index.js
@@ -1,38 +1,5 @@
 var models = require('../models/index'),
-		async = require('async'),
-		trip,
-		station;
-
-var findTrip = function(req) {
-	models.Trip.findOne({bikeId: req.params.bikeId})
-				.sort('-startTime')
-				.skip(req.params.offset)
-				.limit(1)
-				.exec(function(err, trip) {
-					models.Station.findOne({stationId: trip.originStationId})
-								.limit(1)
-								.exec(function(err, station) {
-									console.log(station)
-								});
-				})
-			};
-
-// var findStation = function(req) {
-// 	models.Station.findOne({stationId: trip.originStationId})
-// 				.exec(function(err, data) {
-// 					console.log("station " + data)
-// 					return data
-// 				});
-// 			};
-
-var reply = function() {
-	return {
-		"status": 200,
-		"lat": station.lat,
-		"lng": station.lng
-	}
-	reply(hash)
-}
+		tripResponse = require('./helpers/trip-format');
 
 module.exports = {
 	redirectToApp: {
@@ -42,7 +9,7 @@ module.exports = {
 	},
 	displayTrip: {
 		handler: function(req, reply) {
-	  	findTrip(req);
+	  	tripResponse(req, reply);
 	  }
 	}
 }
